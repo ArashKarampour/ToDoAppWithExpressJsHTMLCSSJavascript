549479c3f4e2db7e91e85bd966e9c94ffc584b4f
Arash Karampour <karampour.arash@yahoo.com>
2022-06-09 02:46:57 +0430

    -getting all the tasks of a user and show them in Today.ejs using panel.js -Today.ejs refactored and redesigned with Today.css -Modal for adding a task with mypanel-today.js and Today.css

--- routes/panel.js
+++ routes/panel.js
@@ -1,14 +1,15 @@
+const {Task} = require("../models/task");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    
-    res.render("Today");
+router.get("/", async (req,res) => {
+    const tasks = await Task.find({userId:req.user._id});
+    res.render("Today",{tasks:tasks});
 });
 
 router.get("/today", (req,res) => {
-    res.render("Today");
+    res.redirect("/api/todo/mypanel/");
 });
 
 
